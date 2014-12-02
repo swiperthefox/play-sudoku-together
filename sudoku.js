684ee908b8f8f017d4278367781456d7827aa6fa
swiperthefox <zhenlei.jia@gmail.com>
2014-12-01 21:06:24 -0500

    correct some error in hangout api usage

--- sudoku.js
+++ sudoku.js
@@ -175,7 +175,7 @@ var CellState = function(i, j) {
 
   self.setValue = function(values) {
     self.values.removeAll();
-    self.values.push.apply(self.values, values);
+    self.values.push.apply(self.values, JSON.parse(values));
   };
   /*
    * Set the state of cell
